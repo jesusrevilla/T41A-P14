52b0ec6dd16d4590fb9d1808e7405589433a73b4
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-03 19:40:33 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE insertar_producto(nombre TEXT, precio NUMERIC)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO productos(nombre, precio)
+    VALUES (nombre, precio);
+END;
+$$;
+
+CALL insertar_producto('Teclado', 299.99);
+CALL insertar_producto('Raton', 99.99);
+
+select * from productos;
