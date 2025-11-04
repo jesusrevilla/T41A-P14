2a82288f1f62ce1c7db7c3756b05a3e60f59e816
VictorCastro135 <178920@upslp.edu.mx>
2025-11-04 13:32:43 -0600

    feature/178920

--- 03_create_procedures.sql
+++ 03_create_procedures.sql
@@ -1,1 +1,16 @@
+CREATE PROCEDURE eliminar_producto(
+    p_id INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM productos
+    WHERE id = p_id;
 
+    IF FOUND THEN
+        RAISE NOTICE 'Producto eliminado exitosamente (ID: %)', p_id;
+    ELSE
+        RAISE NOTICE 'Error: No se encontró un producto con el ID %.', p_id;
+    END IF;
+END;
+$$;
