c9b231fbd02cea1f05130e9544cbbff480a5ef81
Uriel4443 <182829@upslp.edu.mx>
2025-11-03 19:48:09 -0600

    Create PROCEDURE_2.sql

---
+++ PROCEDURE_2.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE porcentaje_precio(
+    IN p_porcentaje NUMERIC
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  UPDATE productos
+  SET precio = precio + precio*p_porcentaje;
+END;
+$$;
+CALL porcentaje_precio(0.1);
+
+SELECT * FROM productos;
