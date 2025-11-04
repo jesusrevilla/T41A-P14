0d9ba0d101a5515ad2baa1d7605204fc399e9a04
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-11-03 19:51:16 -0600

    Update 03_create_procedures.sql

--- 03_create_procedures.sql
+++ 03_create_procedures.sql
@@ -1,15 +1,49 @@
-CREATE PROCEDURE eliminar_producto(
-    IN p_id INT
+CREATE PROCEDURE eliminar_producto(p_id INT)
+LANGUAGE plpgsql 
+AS $$
+DECLARE
+    v_nombre TEXT;
+BEGIN
+    SELECT nombre INTO v_nombre FROM productos WHERE id = p_id;
+    IF NOT FOUND THEN
+        RAISE NOTICE 'Producto no encontrado';
+    ELSE
+        DELETE FROM productos WHERE id = p_id;
+        RAISE NOTICE 'Producto eliminado';
+    END IF;
+END;
+$$;
+
+CREATE PROCEDURE aumentar_precios(
+    p_porcentaje NUMERIC
 )
-LANGUAGE plpgsql
+LANGUAGE plpgsql 
 AS $$
 BEGIN
-    IF p_id IN  THEN
-        UPDATE productos
-        SET precio = p_nuevo_precio
-        WHERE id = p_id;
-    ELSE
-        RAISE NOTICE 'El precio debe ser mayor que cero.';
-    END IF;
+    UPDATE productos
+    SET precio = precio * (1 + (p_porcentaje / 100));
+END;
+$$;
+
+CREATE  PROCEDURE buscar_por_rango(
+    p_minimo NUMERIC,
+    p_maximo NUMERIC
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+    CREATE TEMPORARY TABLE IF NOT EXISTS resultados_productos (
+        id INT,
+        nombre TEXT,
+        precio NUMERIC(10,2)
+    ) ON COMMIT DROP;
+
+    TRUNCATE TABLE resultados_productos;
+
+    INSERT INTO tmp_resultados_productos
+    SELECT *
+    FROM productos
+    WHERE precio BETWEEN p_minimo AND p_maximo
+    ORDER BY precio ASC;
+
 END;
 $$;
