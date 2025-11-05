c3d3f671c35482f78e8c28bee8774cd0efe6bfca
Uriel4443 <182829@upslp.edu.mx>
2025-11-04 19:08:28 -0600

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -3,3 +3,10 @@ CREATE TABLE productos (
   nombre TEXT NOT NULL,
   precio NUMERIC NOT NULL
 );
+
+CREATE TABLE auditoria_productos (
+    id SERIAL PRIMARY KEY,
+    producto_id INT NOT NULL,
+    precio_anterior NUMERIC,
+    precio_nuevo NUMERIC
+);
