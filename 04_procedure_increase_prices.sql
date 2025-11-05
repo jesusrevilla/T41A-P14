91339f49d6f1e76a609745678d7bbe787ade9fcf
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-11-04 19:19:10 -0600

    Create 04_procedure_increase_prices.sql

---
+++ 04_procedure_increase_prices.sql
@@ -0,0 +1,17 @@
+-- Procedimiento para aumentar el precio de todos los productos en un porcentaje
+
+CREATE OR REPLACE PROCEDURE aumentar_precios(IN porcentaje NUMERIC)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF porcentaje <= 0 THEN
+        RAISE NOTICE 'El porcentaje debe ser mayor que cero.';
+        RETURN;
+    END IF;
+
+    UPDATE productos
+    SET precio = precio + (precio * porcentaje / 100);
+
+    RAISE NOTICE 'Precios aumentados en % %', porcentaje, '%';
+END;
+$$;
