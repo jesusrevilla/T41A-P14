12ec6fbf1c4af2668f96cef2f665ba965fdb1796
Diego0086 <178218@upslp.edu.mx>
2025-11-04 19:42:18 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO productos (nombre, precio) VALUES 
+    ('Laptop', 1200.50),
+    ('Teclado', 75.99),
+    ('Mouse', 25.00),
+    ('Monitor', 299.99),
+    ('Silla', 150.75),
+    ('Cafetera', 45.20),
+    ('Audífonos', 89.90),
+    ('Mochila', 55.00),
+    ('Disco Duro', 110.00),
+    ('Webcam', 68.50),
+    ('Router', 99.99),
+    ('Bocinas', 65.40);
