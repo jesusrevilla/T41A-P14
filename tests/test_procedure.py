86b692a412070f5a041043b04ca2810ba0bd0f2b
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-11-04 19:20:34 -0600

    Add tests for stored procedures in test_procedure.py

---
+++ tests/test_procedure.py
@@ -0,0 +1,94 @@
+import psycopg2
+
+def test_procedure_delete():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try: 
+        cur = conn.cursor()
+        cur.execute("CALL eliminar_producto(100);")
+        cur.execute("SELECT 1;")
+        cur.execute("CALL eliminar_producto(3);")
+        cur.execute("SELECT 1;")
+        conn.commit()
+        mensajes=conn.notices
+        assert 'id incorrecto' in mensajes[0]
+        assert 'eliminado correctamente' in mensajes[1]
+        cur.execute("SELECT COUNT(*) FROM productos WHERE id=3;")
+        count = cur.fetchone()[0]
+        assert count == 0
+    except Exception as e:
+        raise e
+    finally:
+        cur.close()
+        conn.rollback()
+        conn.close()
+
+def test_procedure_update():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try:
+        cur = conn.cursor()
+        cur.execute("select precio from productos where id=1");
+        resultadoAnt=cur.fetchone()[0];
+        cur.execute("CALL actualizar_precio(0.15)")
+        conn.commit()
+        cur.execute("select precio from productos where id=1");
+        resultadoAct=cur.fetchone()[0];
+        porcentaje_cambio = (resultadoAct - resultadoAnt) / resultadoAnt
+        assert abs(porcentaje_cambio - 0.15) < 0.0001
+    except Exception as e:
+        raise e
+    finally:
+        cur.close()
+        conn.rollback()
+        conn.close()
+
+"""
+def test_procedure_rango():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    #Se obtienen los registros que devulve el procedure
+    cur.execute("select id,nombre precio from rango_precios(100.00,200.00)");
+    resultados=cur.fetchall();
+    assert len(resultados)==2;
+    cur.close()
+    conn.close()
+
+def test_procedure_auditoria():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute("select count(*) as total from auditoria;");
+    auditoriaAnt=cur.fetchone()[0];
+    cur.execute("update productos set precio=15.0 where id=2;");
+    cur.execute("select count(*) as total from auditoria;");
+    auditoriaAct=cur.fetchone()[0];
+    assert auditoriaAct-auditoriaAnt==1;
+    cur.close()
+    conn.close()
+"""
+
+
