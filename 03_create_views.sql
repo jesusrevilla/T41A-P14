2658337b81302723d19e08247e17615081528d84
Diego0086 <178218@upslp.edu.mx>
2025-11-05 12:53:10 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -43,3 +43,29 @@ $$;
 
 CALL eliminar_producto(1);
 
+CREATE PROCEDURE productos_por_rango(
+    p_precio_min NUMERIC,
+    p_precio_max NUMERIC,
+    INOUT p_cursor REFCURSOR 
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    OPEN p_cursor FOR
+        SELECT
+            p.id,
+            p.nombre,
+            p.precio
+        FROM
+            productos AS p
+        WHERE
+            p.precio BETWEEN p_precio_min AND p_precio_max
+        ORDER BY
+            p.precio;
+END;
+$$;
+
+BEGIN;
+CALL obtener_productos_por_rango_proc(100, 500, 'micursor');
+FETCH ALL FROM "micursor";
+COMMIT;
