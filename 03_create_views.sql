975af08dbb22d826e2d4e953f8fbf8c97f2d4416
Diego0086 <178218@upslp.edu.mx>
2025-11-05 12:05:57 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -26,4 +26,20 @@ $$;
 
 CALL aumentar_precios(10);
 
+CREATE PROCEDURE eliminar_producto(
+  IN p_id INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM productos
+    WHERE Id = p_Id;
+    
+    IF FOUND THEN
+      RAISE NOTICE 'Producto eliminado';
+    END IF;
+END;
+$$;
+
+CALL eliminar_producto(1);
 
