e6725139e4c3b5a8a6603fc3ac56b6fb60d053d2
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-05 23:04:24 -0600

    Update test_procedure.py

--- tests/test_procedure.py
+++ tests/test_procedure.py
@@ -26,11 +26,32 @@ def run_query_from_file(conn, filename):
 
 def test_elimina_por_id(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM productos WHERE id = 1;")
+        cur.execute("SELECT COUNT(*) FROM productos WHERE id = 2;")
         assert cur.fetchone()[0] == 1
-        cur.execute("CALL eliminar_producto(1);")
-        cur.execute("SELECT COUNT(*) FROM productos WHERE id = 1;")
+        cur.execute("CALL eliminar_producto(2);")
+        cur.execute("SELECT COUNT(*) FROM productos WHERE id = 2;")
         assert cur.fetchone()[0] == 0
+
+def test_rango(db_connection):
+    productos_esperados = ['Teclado'] 
+    with db_connection.cursor() as cur:
+        sql_call = """
+        DO $$
+        DECLARE 
+            out_lista TEXT;
+        BEGIN
+            CALL rango_precio(%s, %s, out_lista); 
+            EXECUTE format('SET session rangos.lista_productos = %L', out_lista);
+        END
+        $$;
+        """
+        cur.execute(sql_call, (10, 400))
+        cur.execute("SHOW rangos.lista_productos;")
+        resultado = cur.fetchone()[0]
+        for producto in productos_esperados:
+            assert producto in resultado
+
+
     
 
 
