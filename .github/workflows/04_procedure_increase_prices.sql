6e779ea9a6146b8ca1180c33d1994510f160c172
Karina123Kma <upslpbd@gmail.com>
2025-11-07 15:47:01 -0600

    Create 04_procedure_increase_prices.sql

---
+++ .github/workflows/04_procedure_increase_prices.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE PROCEDURE aumentar_precios( IN p_porcentaje NUMERIC ) LANGUAGE plpgsql AS $$ BEGIN IF p_porcentaje <= 0 THEN RAISE NOTICE 'El porcentaje debe ser mayor que 0.'; RETURN; END IF; UPDATE productos SET precio = precio + (precio * p_porcentaje / 100); RAISE NOTICE 'Precios aumentados en %%.', p_porcentaje; END; $$;
