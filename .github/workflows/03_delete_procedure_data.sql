1a6e6391495e43b9edf82b39427854ef3280b32e
Karina123Kma <upslpbd@gmail.com>
2025-11-07 16:06:51 -0600

    Delete .github/workflows directory

--- .github/workflows/03_delete_procedure_data.sql
+++
@@ -1,1 +0,0 @@
-CREATE OR REPLACE PROCEDURE eliminar_producto( IN p_id INT ) LANGUAGE plpgsql AS $$ BEGIN DELETE FROM productos WHERE id = p_id; IF FOUND THEN RAISE NOTICE 'Producto con ID % eliminado correctamente.', p_id; ELSE RAISE NOTICE 'No existe producto con ID %.', p_id; END IF; END; $$;
