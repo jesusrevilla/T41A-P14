0ac9b19a0a68bc9472aa0a6f194a744627d439f9
Cruz <183213@upslp.edu.mx>
2025-11-09 14:12:03 -0600

    Update 05_busqueda_por_rango.sql

--- 05_busqueda_por_rango.sql
+++ 05_busqueda_por_rango.sql
@@ -1,1 +1,16 @@
+--Crear un procedimiento que reciba un rango de precios y devuelva los productos dentro de ese rango.
 
+CREATE OR REPLACE FUNCTION busqueda_por_rango(p_rangoI NUMERIC, p_rangoF NUMERIC)
+RETURNS TABLE(nombre TEXT, precio NUMERIC)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT p.nombre, p.precio
+  FROM productos p
+  WHERE p.precio > p_rangoI AND p.precio < p_rangoF;
+END;
+$$;
+
+
+SELECT * FROM busqueda_por_rango(10, 1000);
