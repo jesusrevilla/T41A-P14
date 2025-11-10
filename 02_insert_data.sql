513f7e2f36b945b48bbb22e352951a52e54ca991
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-11-10 16:27:04 -0600

    Update product entries in 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,4 +1,5 @@
-INSERT INTO productos(nombre,precio) values 
-('teclado',180.00),
-('mouse',110.00),
-('mousepad',100.00);
+INSERT INTO productos (nombre, precio) VALUES
+('Teclado', 299.99),
+('Mouse', 150.00),
+('Monitor', 1999.99),
+('Laptop', 8500.00);
