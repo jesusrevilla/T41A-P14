a10c66839ac0373b43733a343f868fefd86eb533
patymtz17 <181730@upslp.edu.mx>
2025-11-10 19:10:15 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,6 +1,14 @@
+-- Catálogo de productos disponibles
+CREATE TABLE catalogo_productos (
+    producto_id SERIAL PRIMARY KEY,
+    nombre_producto TEXT NOT NULL,
+    precio_unitario NUMERIC(10,2) NOT NULL
+);
 
-CREATE TABLE productos(
-  id SERIAL PRIMARY KEY,
-  nombre TEXT NOT NULL,
-  precio FLOAT NOT NULL
+-- Registro de acciones sobre productos
+CREATE TABLE log_acciones_productos (
+    log_id SERIAL PRIMARY KEY,
+    producto_id INT NOT NULL,
+    tipo_accion TEXT NOT NULL,
+    fecha_accion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
