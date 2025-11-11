b76c81db67e378d6e991ca7fc5734536d4a65a7d
daf01-web <171513@upslp.edu.mx>
2025-11-11 08:15:17 -0600

    Create 04_test_calls.sql

---
+++ 04_test_calls.sql
@@ -0,0 +1,12 @@
+
+-- Eliminar producto con ID 2
+CALL eliminar_producto(2);
+
+-- Aumentar precios en 10%
+CALL aumentar_precios(10);
+
+-- Mostrar productos entre 200 y 2000
+CALL productos_en_rango(200, 2000);
+
+-- Actualizar precio del producto 1 a 300.00
+CALL actualizar_precio_auditado(1, 300.00);
