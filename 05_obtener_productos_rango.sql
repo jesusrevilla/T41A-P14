b3933e4157c24a8613d4f562bb3e3085b3598f81
Karina123Kma <upslpbd@gmail.com>
2025-11-10 19:24:07 -0600

    Create 05_obtener_productos_rango.sql

---
+++ 05_obtener_productos_rango.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE FUNCTION obtener_productos_rango(precio_min NUMERIC, precio_max NUMERIC) RETURNS TABLE(id INT, nombre VARCHAR, precio NUMERIC, stock INT) AS $$ BEGIN RETURN QUERY SELECT id, nombre, precio, stock FROM productos WHERE precio BETWEEN precio_min AND precio_max ORDER BY precio ASC; END; $$ LANGUAGE plpgsql;
