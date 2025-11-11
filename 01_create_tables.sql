11eb0e7744168c39f01d4a40e8a2461ad79f22a1
daf01-web <171513@upslp.edu.mx>
2025-11-11 08:08:06 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,18 @@
 
+-- Crear tabla de productos
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    precio NUMERIC(10,2) NOT NULL
+);
+
+
+
+-- Tabla de auditoría
+CREATE TABLE auditoria_productos (
+    id SERIAL PRIMARY KEY,
+    producto_id INT,
+    precio_anterior NUMERIC(10,2),
+    precio_nuevo NUMERIC(10,2),
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
