539b77b696ed6689d276e2d0a74200555e890cd1
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-11 21:15:10 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -2,5 +2,5 @@ CREATE TABLE productos (
     id SERIAL PRIMARY KEY,
     nombre TEXT NOT NULL,
     stock INT NOT NULL,
-    precio_unitario NUMERIC(10,2) NOT NULL
+    precio NUMERIC(10,2) NOT NULL
 );
