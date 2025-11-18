0b1e27742df58711251710bf41ff0e3d7dc3bbae
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 20:32:25 -0600

    Update 01_crear_tablas.sql

--- 01_crear_tablas.sql
+++ 01_crear_tablas.sql
@@ -1,1 +1,15 @@
+-- Crear tabla de productos
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    precio NUMERIC NOT NULL
+);
 
+-- Crear tabla de auditoría
+CREATE TABLE auditoria_productos (
+    id SERIAL PRIMARY KEY,
+    producto_id INT,
+    precio_anterior NUMERIC,
+    precio_nuevo NUMERIC,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
