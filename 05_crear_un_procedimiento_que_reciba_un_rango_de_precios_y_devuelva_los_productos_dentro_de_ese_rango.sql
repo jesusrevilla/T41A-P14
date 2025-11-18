c2c859bcbe1961915c449effea2736fcf8332571
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 20:48:06 -0600

    Update 05_crear_un_procedimiento_que_reciba_un_rango_de_precios_y_devuelva_los_productos_dentro_de_ese_rango.sql

--- 05_crear_un_procedimiento_que_reciba_un_rango_de_precios_y_devuelva_los_productos_dentro_de_ese_rango.sql
+++ 05_crear_un_procedimiento_que_reciba_un_rango_de_precios_y_devuelva_los_productos_dentro_de_ese_rango.sql
@@ -1,1 +1,17 @@
+CREATE PROCEDURE productos_en_rango(
+    IN p_min NUMERIC,
+    IN p_max NUMERIC,
+    OUT p_resultados REFCURSOR
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    OPEN p_resultados FOR
+        SELECT * FROM productos
+        WHERE precio BETWEEN p_min AND p_max;
+END;
+$$;
 
+-- Ejemplo de llamada:
+CALL productos_en_rango(100, 1000, 'cur');
+FETCH ALL FROM cur;
