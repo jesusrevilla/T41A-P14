bdc5773c6c89d73c63d4a207267c2e60fea7c4e1
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 20:44:56 -0600

    Update 03_Crear_un_procedimiento_que_elimine_un_producto_por_su_ID_y_devuelva_un_mensaje_si_fue_exitoso_o_no.sql

--- 03_Crear_un_procedimiento_que_elimine_un_producto_por_su_ID_y_devuelva_un_mensaje_si_fue_exitoso_o_no.sql
+++ 03_Crear_un_procedimiento_que_elimine_un_producto_por_su_ID_y_devuelva_un_mensaje_si_fue_exitoso_o_no.sql
@@ -1,1 +1,19 @@
+CREATE PROCEDURE eliminar_producto(
+    IN p_id INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM productos
+    WHERE id = p_id;
 
+    IF FOUND THEN
+        RAISE NOTICE 'Producto con ID % eliminado correctamente.', p_id;
+    ELSE
+        RAISE NOTICE 'No se encontró producto con ID %.', p_id;
+    END IF;
+END;
+$$;
+
+-- Ejemplo de llamada:
+-- CALL eliminar_producto(2);
