6baa21b0ea3cabd9806c6fd272e4f492b7c74687
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-23 20:58:00 -0600

    Insert multiple products into catalogo_productos

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO catalogo_productos (nombre_producto, precio_unitario) VALUES
+('Teclado', 100.00),
+('Mouse', 50.00),
+('Monitor', 600.00),
+('Laptop', 1500.00),
+('Auriculares', 80.00);
