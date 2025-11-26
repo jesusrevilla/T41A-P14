bc691ba10167fa622d935574895b0150b2614212
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 22:43:16 -0600

    Create 01_create_tables.sql

---
+++ 01_create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE productos(
+  id SERIAL,
+  nombre TEXT,
+  precio NUMERIC(10,2)
+  );
+
+CREATE TABLE IF NOT EXISTS productos_auditoria (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_producto INT,
+    nombre_anterior TEXT,
+    precio_anterior NUMERIC(10,2),
+    nombre_nuevo TEXT,
+    precio_nuevo NUMERIC(10,2),
+    usuario_db TEXT DEFAULT current_user,
+    fecha_cambio TIMESTAMP DEFAULT now()
+);
